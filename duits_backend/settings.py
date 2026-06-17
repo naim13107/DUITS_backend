9cd624bf932f9d190f852f5a65eb459acf47369b
MD.Naim-UL-Haque <naimulh644@gmail.com>
2026-06-17 13:19:21 +0600

    v1.1.05

--- duits_backend/settings.py
+++ duits_backend/settings.py
@@ -146,8 +146,8 @@ USE_I18N = True
 USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:8000",
     
     # You can add your production frontend URL here later once it is live!
     # "https://www.duits.com", 
