c67ee90bec304ca9985a11170ce8c8930b86c1f8
MD.Naim-UL-Haque <naimulh644@gmail.com>
2026-06-29 02:06:01 +0600

    member delete

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+class CustomUserViewSet(UserViewSet):
+    def destroy(self, request, *args, **kwargs):
+        if request.user.is_staff or request.user.is_superuser:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
