c8880934df1fd6174b33bbf02ec8309e7914a7dc
MD.Naim-UL-Haque <naimulh644@gmail.com>
2026-07-01 02:13:38 +0600

    member delete 5

--- users/views.py
+++ users/views.py
@@ -1,11 +1,23 @@
+# users/views.py
 from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.response import Response
 
 class CustomUserViewSet(UserViewSet):
     def destroy(self, request, *args, **kwargs):
-        if request.user.is_staff or request.user.is_superuser:
+        requester = request.user
+        is_admin = (
+            requester.is_staff or 
+            requester.is_superuser or 
+            getattr(requester, 'role', '') == 'Admin'
+        )
+        if is_admin:
             instance = self.get_object()
+            if instance == requester:
+                return Response(
+                    {'detail': "You can't delete your own account here."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
             instance.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         return super().destroy(request, *args, **kwargs)
